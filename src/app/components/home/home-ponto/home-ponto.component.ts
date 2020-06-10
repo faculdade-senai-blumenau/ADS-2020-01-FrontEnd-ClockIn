43559d2fd674b86e2493c6b0f633597d9d02b618
raulnoering <45273284+raulnoering@users.noreply.github.com>
2020-06-09 22:16:16 -0300

    ADS-78

--- src/app/components/home/home-ponto/home-ponto.component.ts
+++ src/app/components/home/home-ponto/home-ponto.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home-ponto',
   templateUrl: './home-ponto.component.html',
   styleUrls: ['./home-ponto.component.css']
 })
 export class HomePontoComponent implements OnInit {
+  postId;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
+  async registrarPonto(){
+    const route="posts";
+    var loader=document.getElementById("loaderBtn");
+    var btn=document.getElementById("registrarBtn");
+    btn.style=loader.style='display:none';
+    loader.style='display:inline';
+    await this.delay(500);
+    this.http.post<any>('http://localhost:3001/'+route, { idUsuario: 'idDoUsuario' }).subscribe(data => {
+          this.postId = data.id;
+      })
+    btn.style=loader.style='display:inline';
+    loader.style='display:none';
+  }
+   delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  } 
   ngOnInit() {
   }
 
 }
+
+
+
+
+  
