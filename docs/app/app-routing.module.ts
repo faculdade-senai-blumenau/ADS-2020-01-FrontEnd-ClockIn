95542b593911f51953ef4def1713e8628cd521db
allexxandre <viana.alexandre@hotmail.com>
2020-06-11 20:17:37 -0300

    Build Projeto 11/06/2020

--- docs/app/app-routing.module.ts
+++ docs/app/app-routing.module.ts
@@ -1,24 +1,27 @@
 
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { EditarMarcacaoComponent } from './components/editar-marcacao/editar-marcacao.component';
-import { EspelhoPontoComponent } from './components/espelho-ponto/espelho-ponto.component';
 import { RelatoriosComponent } from './components/relatorios/relatorios.component';
 import { AprovacoesPendentesComponent } from './components/aprovacoes-pendentes/aprovacoes-pendentes.component';
 import { CadastrosComponent } from './components/cadastros/cadastros.component';
 import { ParametroComponent } from './components/parametro/parametro.component';
-
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { EspelhoPontoComponent } from './components/espelho-ponto/espelho-ponto.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'editarmarcacao', component: EditarMarcacaoComponent },
   { path: 'cadastros', component: CadastrosComponent },
   { path: 'espelhoponto', component: EspelhoPontoComponent },
   { path: 'relatorios', component: RelatoriosComponent },
   { path: 'aprovacoespendentes', component: AprovacoesPendentesComponent },
   { path: 'parametros', component: ParametroComponent },
+  { path: 'pagenotfound', component: PagenotfoundComponent },
+  { path: '**', redirectTo: 'pagenotfound' }
 ];
 
 @NgModule({
