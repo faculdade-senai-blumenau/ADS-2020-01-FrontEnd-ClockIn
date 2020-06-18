1d4e57d12ff197de2cf03eb296715b2ab7b31327
allexxandre <viana.alexandre@hotmail.com>
2020-06-17 22:56:44 -0300

    Publicação de Versão

--- docs/app/app-routing.module.ts
+++ docs/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
   { path: 'aprovacoespendentes', component: AprovacoesPendentesComponent },
   { path: 'parametros', component: ParametroComponent },
   { path: 'pagenotfound', component: PagenotfoundComponent },
-  { path: '**', redirectTo: 'home' } 
+  { path: '*', redirectTo: 'home' } 
 ];
  
 @NgModule({
