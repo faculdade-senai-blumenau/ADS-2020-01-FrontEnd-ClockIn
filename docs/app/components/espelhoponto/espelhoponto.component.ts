21c112caa26bd61ff7795c747f67849d7b2826d5
brc.alexandre.viana <alexandre.viana@brconselhos.com>
2020-06-30 15:30:55 -0300

    Correções

---
+++ docs/app/components/espelhoponto/espelhoponto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-espelhoponto',
+  templateUrl: './espelhoponto.component.html',
+  styleUrls: ['./espelhoponto.component.css']
+})
+export class EspelhopontoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
