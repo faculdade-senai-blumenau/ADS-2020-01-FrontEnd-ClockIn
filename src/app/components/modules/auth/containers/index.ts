a9614e545e6092bde88d3cbcfd2443ff68b3db22
allexxandre <viana.alexandre@hotmail.com>
2020-07-01 03:37:27 -0300

    Ajustes Tabela Editar Ponto

--- src/app/components/modules/auth/containers/index.ts
+++
@@ -1,7 +0,0 @@
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { LoginComponent } from './login/login.component';
-
-export const containers = [LoginComponent, ForgotPasswordComponent];
-
-export * from './login/login.component';
-export * from './forgot-password/forgot-password.component';
